f65bad276e10b61101d944cb18e71e3844d721d0
SAYAN <sayandey1203@gmail.com>
2026-03-02 23:12:58 +0530

    navbar complete for desktop and menu symbol for mobile

---
+++ src/components/navbar.tsx
@@ -0,0 +1,91 @@
+import Link from "next/link";
+import { useEffect , useState} from "react";
+import { toast} from "react-hot-toast";
+import { usePathname} from "next/navigation";
+import { Sun , Moon , X , Loader2 , Menu} from "lucide-react";
+import { useTheme } from "next-themes";
+import {Button} from "@/components/ui/button";
+
+
+
+export default function Navbar() {
+    const {theme , setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+    const [menuOpen , setMenuOpen] = useState(false);
+
+
+    useEffect(() => {
+    setMounted(true);
+    }, []);
+
+    if (!mounted) return null;
+
+    const pathname = usePathname();
+    
+    const isActive = (path: string) => {
+        return pathname === path ? "text-accent text-semibold" : "text-foreground"
+    }
+
+    const handleComingSoon = () => {
+        toast("🚧 Coming Soon!")
+    }
+
+
+    return(
+        <>
+        <nav
+            className = "w-full sticky top-0 z-50 border-b border-border bg-background/70 shadow-sm backdrop-blur-2xl"
+        >
+            <div className="max-w-5xl mx-auto px-4 py-4 flex items-center justify-between">
+                {/* main name */}
+                <Link href="/" 
+                    className="text-xl font-bold text-foreground hover:text-accent transition-colors"
+                >
+                     Sayan Dey
+                </Link>
+
+                {/*menu for desktop*/}
+                <div className="hidden md:flex items-center space-x-6 text-sm">
+
+                    <Link href="/blogs"
+                        className={`hover:text-accent transition-colors ${isActive("/blogs")}`}
+                    > 
+                        Blogs
+                    </Link>
+                    <button
+                        onClick={handleComingSoon}
+                        className={`hover:text-accent transition-colors ${isActive("/projects")}`}
+                        >
+                        Projects
+                    </button>
+                    <button
+                        onClick={handleComingSoon}
+                        className={`hover:text-accent transition-colors ${isActive("/ai-lab")}`}
+                        >
+                        AI Lab
+                    </button>
+
+                    {/* toggle button for theme */}
+                    <Button
+                        variant="outline"
+                        size="icon"
+                        className="rounded-full"
+                        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+                    >
+                        { theme === "light" ? ( <Moon className="h-5 w-5"/>)
+                        :( <Sun className="h-5 w-5"/>)
+                    }    
+                    </Button>
+
+                </div>
+                {/* X for mobile */}
+                <button className="md:hidden" 
+                    onClick={() => setMenuOpen(!menuOpen)}>
+                        {!menuOpen ? <Menu className="w-6 h-6"/> : <X className="w-6 h-6"/>}
+                    </button>
+            </div>
+
+        </nav>
+        </>
+    );
+}
