daaed169c9126b42190874d36f1734696c245de0
SAYAN <sayandey1203@gmail.com>
2026-03-03 20:10:25 +0530

    fixes layout and add suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,9 +53,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground flex flex-col min-h-screen overflow-hidden`}
       >
         <ThemeProvider
             attribute="class"
