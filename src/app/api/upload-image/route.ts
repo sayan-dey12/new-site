64ed247fe7e8531cddd1fb75b8e8a777fa19dca3
SAYAN <sayandey1203@gmail.com>
2026-03-09 19:47:30 +0530

    make the upload-image api more robust

--- src/app/api/upload-image/route.ts
+++ src/app/api/upload-image/route.ts
@@ -1,20 +1,34 @@
-import cloudinary from "@/lib/cloudinary"
+import cloudinary from "@/lib/cloudinary";
 
-export async function POST(req: Request) {
-  const formData = await req.formData()
+export async function POST(req: Request){
+  try {
+    const formData = await req.formData()
+    const file = formData.get("file") as File | null;
+    if(!file){
+      return Response.json({
+        success: false,
+        error: "No file uploaded"
+      },{status: 400})
+    }
+    if(!file.type.startsWith("image/")){
+      return Response.json({
+          success: false,
+          error: "Invalid File Type"
+        },{status: 400})
+    }
+    const buffer = Buffer.from(await file.arrayBuffer())
 
-  const file = formData.get("file") as File
-
-  const buffer = Buffer.from(await file.arrayBuffer())
-
-  const result = await new Promise((resolve, reject) => {
-    cloudinary.uploader
-      .upload_stream({ folder: "blogs" }, (error, result) => {
-        if (error) reject(error)
-        else resolve(result)
-      })
-      .end(buffer)
-  })
-
-  return Response.json(result)
+    const result : any = await new Promise((resolve , rejects)=>{
+      cloudinary.uploader.upload_stream({folder: "new-site"} , (error , res)=>{
+        if (error) rejects(error)
+        else resolve(res)
+      }).end(buffer)
+    })
+    return Response.json({ url: result.secure_url });
+  } catch (error) {
+      return Response.json({ 
+        status: false,
+        error: "Upload failed" }, 
+        { status: 500 })
+  }
 }
