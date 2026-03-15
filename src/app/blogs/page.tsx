9bb00711ad1ad6c58025a7553ecd9a76ef75da90
SAYAN <sayandey1203@gmail.com>
2026-03-15 23:39:03 +0530

    fix the 6 blog issue in blogs page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -7,7 +7,7 @@ import { BlogType } from "@/types/blog";
 
 export default async function BlogsPage() {
   
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blog?limit=6`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blog`, {
     cache: "no-store"
   });
 
