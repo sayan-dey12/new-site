4d5cdacf7bd628c1612c7d9332b48a922322e1fe
SAYAN <sayandey1203@gmail.com>
2026-03-16 17:01:43 +0530

    fix a word

--- src/app/admin/(dashboard)/dashboard/ai-lab/new/page.tsx
+++ src/app/admin/(dashboard)/dashboard/ai-lab/new/page.tsx
@@ -3,7 +3,7 @@ import AiLabEditor from "@/components/ai-lab/AiLabEditor";
 export default function NewAiLabPage(){
     return(
         <div className="max-w-5xl mx-auto py-10">
-              <h1 className="text-3xl font-bold mb-8">Add New Project</h1>
+              <h1 className="text-3xl font-bold mb-8">Add New AI Project</h1>
                 <AiLabEditor/>
             </div>
     )
