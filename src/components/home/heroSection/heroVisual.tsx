076d0de05bc447ac54e5a6ef6058fad22f84fc8c
SAYAN <sayandey1203@gmail.com>
2026-03-18 18:14:38 +0530

    add securities in the hero image , no drag , right click , highlighting , easy select

--- src/components/home/heroSection/heroVisual.tsx
+++ src/components/home/heroSection/heroVisual.tsx
@@ -1,30 +1,13 @@
+"use client";
+
 import Image from "next/image";
 
 export default function HeroVisual() {
   return (
     <div className="relative isolate flex items-center justify-center">
 
-      {/* Accent Glow */}
-      {/* <div
-        className="
-          absolute
-          -z-10
-          w-80 h-80
-          md:w-[420px] md:h-[420px]
-          bg-accent/30
-          blur-3xl
-          rounded-full
-        "
-      /> */}
+      <div className="relative rounded-3xl overflow-hidden">
 
-      {/* Image Card */}
-      <div
-        className="
-          relative
-          rounded-3xl
-          overflow-hidden
-        "
-      >
         {/* 🖼️ Image */}
         <Image
           src="/hero_image_3.png"
@@ -32,24 +15,33 @@ export default function HeroVisual() {
           width={420}
           height={520}
           priority
-          className="object-cover w-70 md:w-95 h-auto"
+          draggable={false}
+          onDragStart={(e) => e.preventDefault()}
+          onContextMenu={(e) => e.preventDefault()}
+          className="object-cover w-70 md:w-95 h-auto select-none"
         />
 
-        {/* 🌫️ Bottom Fade (FIXED POSITION) */}
-          <div
-            className="
-              pointer-events-none
-              absolute inset-x-0 bottom-0
-              h-28 md:h-40
-              bg-linear-to-b
-              from-transparent
-              via-[#fff7ea]/20
-              to-[#fff7ea]
-              dark:via-[#0b0a0a]/40 dark:to-[#0b0a0a]
-            "
-          />
-      </div>
+        {/* 🔒 Transparent Overlay (Protection Layer) */}
+        <div
+          className="absolute inset-0 z-10"
+          onContextMenu={(e) => e.preventDefault()}
+        />
 
+        {/* 🌫️ Bottom Fade */}
+        <div
+          className="
+            pointer-events-none
+            absolute inset-x-0 bottom-0
+            h-28 md:h-40
+            bg-linear-to-b
+            from-transparent
+            via-[#fff7ea]/20
+            to-[#fff7ea]
+            dark:via-[#0b0a0a]/40 dark:to-[#0b0a0a]
+          "
+        />
+
+      </div>
     </div>
   );
 }
