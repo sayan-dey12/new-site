c8f3eebcc47fc606c059031246dbdda616d7fda8
SAYAN <sayandey1203@gmail.com>
2026-03-18 20:57:40 +0530

    do like previous but change the env

--- src/app/admin/(dashboard)/connect/page.tsx
+++ src/app/admin/(dashboard)/connect/page.tsx
@@ -8,10 +8,9 @@ type Contact = {
 };
 
 export default async function ContactsPage() {
-  const baseUrl =
-  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
-    const res = await fetch(`/api/admin/contacts`, {
+    const res = await fetch(`${baseUrl}/api/admin/contacts`, {
     cache: "no-store",
     });
 
